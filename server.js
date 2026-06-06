d162c05b1142270b69234d0a749124287d436933
Allysson2023 <allyssoncarlos.ac21@gmail.com>
2026-06-06 13:08:47 -0300

    Reajuste em algumas pasta

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const helmet = require('helmet');
 const cors = require('cors');
 
 app.use(helmet());
-app.use(cors());
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use('/uploads', express.static('uploads'));
