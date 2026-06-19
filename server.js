ef0e2ef350c5bac230ad8ecc4739a0cf459f4792
Allysson2023 <allyssoncarlos.ac21@gmail.com>
2026-06-19 09:54:09 -0300

    Reajuste nas portas

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(
 );
 
 const allowedOrigins = [
-  "http://localhost:5173", 
+    "http://localhost:5173",
   "https://vandoraapp.netlify.app" // SUBSTITUA PELA SUA URL REAL DO NETLIFY
 ];
 
